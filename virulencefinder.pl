a35ae635b2b91807fc88d15a7cbb10e792c0bda7
Rosa Lundbye Allesøe <rosa@cbs.dtu.dk>
2016-10-05 17:14:19 +0200

    update of perl script to not use tmpfile/tmpdir

--- virulencefinder.pl
+++ virulencefinder.pl
@@ -6,7 +6,6 @@
 
 use strict;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
-use File::Temp qw/ tempfile tempdir /;
 use Bio::SeqIO;
 use Bio::Seq;
 use Bio::SearchIO;
@@ -48,6 +47,9 @@ if (not defined $dir) {
   $dir = ".";
 }
 
+# Making tmp directory for BLAST output
+my $tmp_dir = "$dir/tmp";
+mkdir("$dir/tmp");
 
 # --------------------------------------------------------------------
 
@@ -56,16 +58,13 @@ if (not defined $dir) {
 ## AVAILABLE antimicrobial familyes ##
   # hash mapping the ARGF profiles to antibiotic names
 my %argfProfiles=();
-##################### Antimicrobial familyes ################# ="Aminoglycoside";
-#$argfProfiles{"virulence_vtec"} ="VTEC";
-#$argfProfiles{"virulence_vtec_extended"} ="Other virulence genes - E. Coli";
-#$argfProfiles{"virulence_hhas"} ="Virulence - TEST HHAS";
-#$argfProfiles{"virulence_fmaa"} ="Virulence - TEST FMAA";
-$argfProfiles{"virulence_ENT"} ="Virulence - Enterococcus";
-$argfProfiles{"virulence_ecoli"} ="Virulence - E. coli";
-$argfProfiles{"s.aureus_enterotoxin"} ="S. aureus - Enterotoxin";
-$argfProfiles{"s.aureus_exoenzyme"} ="S. aureus - Exoenzyme";
-$argfProfiles{"s.aureus_hostimm"} ="S. aureus - Host Immune evasion";
+open(IN, '<', "$ABRES_DB/config") or die "Error: $!\n";
+while (defined(my $line = <IN>)) {
+   next if $line =~ m/^#/;  # discard comments
+   my @tmp = split("\t", $line);
+   $argfProfiles{$tmp[0]} = $tmp[1];
+}
+close IN;
 #$argfProfiles{"s.aureus"} ="S. aureus";
 # -----------------------
 #Making phenotype list
@@ -108,7 +107,7 @@ foreach my $element(@Antimicrobial){
       $Seqs_input  = $InFile ne "" ? read_seqs(-file => $InFile, -format => $IFormat) : 
                                   read_seqs(-fh => \*STDIN,   -format => $IFormat);
    
-      @Blast_lines = get_blast_run(-d => $Seqs_input, -i => $Seqs_ABres, %ARGV)
+      @Blast_lines = get_blast_run($tmp_dir, $element, -d => $Seqs_input, -i => $Seqs_ABres, %ARGV)
    }
    catch{ die $_ };
  
@@ -1021,6 +1020,10 @@ open (ALLELE, '>'."$dir/Virulence_gene_seq.fsa") || die("Error! Could not write 
 print ALLELE $resalign;
 close (ALLELE);
 
+system("rm -r error.log");
+system("rm -r formatdb.log");
+system("rm -r  $tmp_dir/");
+
 exit;
 
 
@@ -1096,60 +1099,59 @@ sub phenolist {
 # Returns text lines of blast output
 
 sub get_blast_run {
-  my %args        = @_;
-  my ($fh, $file) = tempfile( DIR => '/tmp', UNLINK => 1); 
-  #print "\ntemp filename: $file\n";
-  output_sequence(-fh => $fh, seqs => delete $args{-d}, -format => 'fasta');
-  die "Error! Could not build blast database" if (system("$FORMATDB -p F -i $file")); 
-  my $query_file = $file.".blastpipe";
+   my ($tmp_dir, $org, %args) = @_;
+   #my $fh = $tmp_dir;
+   my $file = "blast_$org.fsa";
+   #my ($fh, $file) = tempfile( DIR => '/tmp', UNLINK => 1);
+   output_sequence(-file => ">$tmp_dir/$file", seqs => delete $args{-d}, -format => 'fasta');
+   die "Error! Could not build blast database" if (system("$FORMATDB -p F -i $tmp_dir/$file"));
+	
+   my $query_file = "$file.blastpipe";
 
-  open QUERY, ">> $query_file" || die("Error! Could not perform blast run");
-  output_sequence(-fh => \*QUERY, seqs => $args{-i}, -format => 'fasta');
-  close QUERY;
+   #open QUERY, ">> $query_file" || die("Error! Could not perform blast run");
+   output_sequence(-file => ">$tmp_dir/$query_file", seqs => $args{-i}, -format => 'fasta');
+   #close QUERY;
+   
+   delete $args{-i};
 
-  delete $args{-i};
-  my $cmd = join(" ", %args);
-  my ($fh2, $file2) = tempfile( DIR => '/tmp', UNLINK => 1); 
-  #print "\ntemp filename: $file2\n";
-  print $fh2 `$BLASTALL -d $file -i $query_file $cmd`;
-  close $fh2;
-  #system("$BLASTALL -d $file -i $query_file $cmd > $file2");
-  #system ("echo -d $file -i $query_file $cmd ");
-  my $report = new Bio::SearchIO(
-         -file => $file2,
-              -format => "blast"); 
-# Go through BLAST reports one by one, evt. array in array...              
-my @blast;
-while(my $result = $report->next_result) {
-   # Go through each matching sequence
-   while(my $hit = $result->next_hit)    { 
-      # Go through each HSP for this sequence
-        while (my$hsp = $hit->next_hsp)  { 
-            push(@blast, 
-      $result->query_accession . "\t" . 
-      $result->query_length . "\t" . 
-      $hsp->hsp_length . "\t" . 
-      $hsp->gaps . "\t" . 
-      $hsp->percent_identity .  "\t" .  
-      $hsp->evalue . "\t" . 
-      $hsp->bits . "\t" . 
-      $hsp->query_string ."\t" . 
-      $hsp->hit_string ."\t" . 
-      $hsp->homology_string . "\t" . 
-      $hsp->seq_inds . "\t" .
-      $hsp->strand('hit') . "\t" .
-      $hsp->start('hit') . "\t" .
-      $hsp->end('hit') . "\t" .
-      $hit->name . "\t" .
-      $hsp->strand('query') . "\t" .
-      $hsp->start('query') . "\n");
-      } 
-   } 
+   my $cmd = join(" ", %args);
+   my $file2 = "$tmp_dir/$file.blast_output";
+   system("$BLASTALL -d $tmp_dir/$file -i $tmp_dir/$query_file -o $file2 $cmd");
+
+   my $report = new Bio::SearchIO( -file   => $file2,
+                                   -format => "blast"
+                                 );
+   # Go through BLAST reports one by one
+   my @blast;
+   while(my $result = $report->next_result) {
+      # Go through each matching sequence
+      while(my $hit = $result->next_hit)    {
+         # Go through each each HSP for this sequence
+         while (my$hsp = $hit->next_hsp)  {
+            push(@blast, $result->query_accession ."\t".
+                        $result->query_length ."\t".
+                        $hsp->hsp_length ."\t".
+                        $hsp->gaps ."\t".
+                        $hsp->percent_identity ."\t".
+                        $hsp->evalue ."\t".
+                        $hsp->bits ."\t".
+                        $hsp->query_string ."\t".
+                        $hsp->hit_string ."\t".
+                        $hsp->homology_string ."\t".
+                        $hsp->seq_inds ."\t".
+                        $hsp->strand('hit') ."\t".
+                        $hsp->start('hit') ."\t".
+                        $hsp->end('hit') ."\t".
+                        $hit->name ."\t".
+                        $hsp->strand('query') ."\t".
+                        $hsp->start('query') ."\t".
+                        $hit->length ."\n");
+         }
+      }
+   }
+   return @blast;
 }
-unlink 'formatdb.log', 'error.log', "$file.blastpipe" , "$file.nhr" , "$file.nin" , "$file.nsq";
-#system("rm -rf formatdb.log error.log");
-return @blast;
-}
+
 
 ###################################
 # Finds sequences with specific ids in an array of Bio::Seq objects
@@ -1226,22 +1228,24 @@ sub Getting_gaps {
 #   The filehandle and filename
 
 sub output_sequence {
-  my %args = @_;
-  my $seqs_ref = delete $args{seqs};
-  my $i = 1;
-  $args{-fh} = \*STDOUT unless (exists $args{-fh} or exists $args{-file});
-  if (exists $args{tempdir}) {
-    my $tempdir = delete $args{tempdir};
-    ($args{-fh}, $args{-file}) = tempfile(DIR => $tempdir, SUFFIX => ".".$args{-format})
-  }
-  my $file = delete $args{-file} if (exists $args{-fh} && exists $args{-file}); # Stupid BioPerl cannot handle that both might be set...
-  my $seq_out = Bio::SeqIO->new(%args);
-  $args{-file} = $file if (defined $file);
-  for my $seq (@{ $seqs_ref }) {
-    $seq_out->write_seq($seq);
-  }
-  return ($args{-fh}, $args{-file});
+   my %args = @_;
+   my $seqs_ref = delete $args{seqs};
+   my $i = 1;
+   #$args{-fh} = \*STDOUT unless (exists $args{-fh} or exists $args{-file});
+   #if (exists $args{tempdir}) {
+   #   my $tempdir = delete $args{tempdir};
+   #   ($args{-fh}, $args{-file}) = tempfile(DIR => $tempdir, SUFFIX => ".".$args{-format})
+   #}
+   my $file = delete $args{-file} if (exists $args{-fh} && exists $args{-file}); # Stupid BioPerl cannot handle that both might be set...
+   print %args;
+   my $seq_out = Bio::SeqIO->new(%args);
+   $args{-file} = $file if (defined $file);
+   for my $seq (@{ $seqs_ref }) {
+      $seq_out->write_seq($seq);
+   }
+   return ($args{-fh}, $args{-file});
 }
+
 
 # --------------------------------------------------------------------
 # %% Help Page/Documentation %%
